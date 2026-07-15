c4cacf146f971ce51fd857500e7a1b77861870c6
wilfried bako <bakowilfriedmarcel@gmail.com>
2026-07-14 21:19:02 -0400

    save reason

--- LABS/lab01-python-introduction/hello.py
+++ LABS/lab01-python-introduction/hello.py
@@ -3,4 +3,9 @@ print("Welcome to Python programming.")
 
 name = input("What is your name? ")
 
-print("Hello, " + name + "! Glad to have you learning Python.")
+print(f"Hello,  {name}! Glad to have you learning Python.")
+ 
+first = "Wilfried"
+last = "Bako"
+print(len("Python"))
+print(first + last)
